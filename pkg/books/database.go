a1803716c5813c3991cde96e4bff0d639505da7e
Falko Benthin <Falko.Benthin@hpi.de>
2019-01-08 14:31:21 +0100

    error checks added

--- pkg/books/database.go
+++ pkg/books/database.go
@@ -49,20 +49,19 @@ func (sbd SQLBookDatabase) Initialize() error {
 
 	var schema = `CREATE TABLE books (isbn text, title text, author text);`
 
-//	_, err := sbd.db.Exec(schema)
-//	if err != nil {
-//		return err
-//	}
+	_, err := sbd.db.Exec(schema)
+	if err != nil {
+		return err
+	}
 	sbd.db.Exec(schema)
 
 	var firstBooks = `INSERT INTO books (isbn, title, author) VALUES ('978-1789619270', 'Kubernetes Design Patterns and Extensions', 'Onur Yilmaz'),  
 																	 ('B07HHDVSJK', 'Cloud-Native Continuous Integration and Delivery', 'Onur Yilmaz')`
 
-//	_, err = sbd.db.Exec(firstBooks)
-//	if err != nil {
-//		return err
-//	}
-	sbd.db.Exec(firstBooks)
+	_, err = sbd.db.Exec(firstBooks)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
